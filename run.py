befbf929782339441cb75663de6c8a2fd84cbf83
cgrace0044 <>
2024-01-23 14:17:50 +0000

    create class for win and hand scenarios

--- run.py
+++ run.py
@@ -19,3 +19,16 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 # data = sales.get_all_values()
 
 # print(data)
+
+# Class for win scenarios
+class Result(enum.Enum):
+    player_one_wins = enum.auto()
+    player_two_wins = enum.auto()
+    draw = enum.auto()
+
+# Class for hand scenarios
+class Hand(enum.Enum):
+    paper = enum.auto()
+    rock = enum.auto()
+    scissors = enum.auto()
+
