865a717e193024777a93632cce43f8cad652e0a5
cgrace0044 Grace <cgrace0044@gmail.com>
2024-02-02 14:23:49 +0000

    add better commentary to pytest

--- test_game.py
+++ test_game.py
@@ -2,6 +2,9 @@ from run import decide_who_wins, Hand, Result
 
 
 def test_decide_who_wins():
+    """
+    Add a test to ensure that the correct result is displaying for each game.
+    """
     assert decide_who_wins(Hand.paper, Hand.rock) == Result.player_one_wins
     assert decide_who_wins(Hand.rock, Hand.rock) == Result.draw
     assert decide_who_wins(Hand.scissors, Hand.rock) == Result.player_two_wins
